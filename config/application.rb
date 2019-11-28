3d42cbfa61eac9294815d30fc0cd7c21f05fdb5c
assouev <bsngr.asv@gmail.com>
2019-11-28 16:25:46 +0100

    debug hero

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 config.assets.initialize_on_precompile = false
 
-module PussyPics
+module PussyPicss
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
